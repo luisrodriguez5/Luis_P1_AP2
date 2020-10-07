44c771169241fe5b638adb89d2a55ae1afd6afd4
luisrodriguez5 <luis_rodriguez5@ucne.edu.do>
2020-10-07 19:06:28 -0400

    Creando la base de datos y migracion

---
+++ Migrations/20201007230149_Initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Luis_P1_AP2.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "producto",
+                columns: table => new
+                {
+                    ProductoId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(nullable: false),
+                    Existencia = table.Column<string>(nullable: false),
+                    Costo = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_producto", x => x.ProductoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "producto");
+        }
+    }
+}
